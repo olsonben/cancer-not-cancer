9bdd5b49849456cb2bbbba1d84883023af85679b
blob1234567890 <ben.k.olson@gmail.com>
2022-03-31 07:15:11 -0700

    this has errors

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -2,8 +2,27 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5050;
 
+const imageNameList = [
+    'peacock-feather',
+    'bridge',
+    'butterfly',
+    'leaf-path',
+    'orange-slice',
+    'tree'
+]
+
+let resHistory = {}
+
 app.get('/', (req, res) => {
-    res.send('Hello, World!')
+    res.send({imageName: imageNameList[Math.floor(Math.random() * imageNameList.length)]})
+    console.log("get success");
+})
+
+app.post('/', (req, res) => {
+    for (const values of req.history) {
+        resHistory.push(values);
+    }
+    console.log("post success");
 })
 
 app.listen(port, () => {
