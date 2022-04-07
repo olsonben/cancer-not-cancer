5c78f840ba8b9e745b3287808f6bf5d5ee67570f
blob1234567890 <ben.k.olson@gmail.com>
2022-04-07 10:00:07 -0700

    commenting and cleaning up code

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -2,12 +2,14 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5050;
 
+// This is vital to parsing the requests
 const bodyParser = require('body-parser')
 app.use(bodyParser.json());       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
     extended: true
 })); 
 
+// this should match the name names of the images in assets
 const imageNameList = [
     'peacock-feather',
     'bridge',
@@ -17,20 +19,23 @@ const imageNameList = [
     'tree'
 ]
 
+// TODO: store the history as a solid database, not holding it in memory
+// TODO: come up with a better term that 'history' for the responses from the pathologist. `pathHistory`?
 let resHistory = {}
 
 app.get('/images', (req, res) => {
-    console.log("Express server: /images");
+    console.log("Express server: /images"); // tracking location
 
-    res.send(imageNameList[Math.floor(Math.random() * imageNameList.length)])
+    res.send(imageNameList[Math.floor(Math.random() * imageNameList.length)]) // send a random image name
 })
 
 app.post('/history', (req, res) => {
     console.log("Express server: /history")
 
+    // REMEMBER: the data in body is in JSON format
     console.log("Joe Stuff " + req.body.myParameter)
 
-    res.send(req.body.myParameter.toUpperCase())
+    res.send(req.body.myParameter.toUpperCase()) //mutate the request data and send it back to the website
 })
 
 app.listen(port, () => {
