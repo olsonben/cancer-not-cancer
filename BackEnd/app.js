220891c79c863860df4e66d2d557b8cadeef6b3d
blob1234567890 <ben.k.olson@gmail.com>
2022-04-11 11:56:33 -0700

    successfully getting and displaying images from the server to the client

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -3,7 +3,8 @@ const app = express()
 const port = process.env.PORT || 5050;
 const path = require('path')
 // This is vital to parsing the requests
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const { pathToFileURL } = require('url');
 app.use(bodyParser.json());       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
     extended: true
@@ -25,11 +26,9 @@ let pathHistory = []
 app.get('/images', (req, res) => {
     console.log("Express server: /images"); // tracking location
 
-    let imageName = imageNameList[Math.floor(Math.random() * imageNameList.length)]
-    let imagePath = path.join(__dirname, '/images/' + imageName + '.jpeg')
-
-    res.sendFile(imagePath)
-    // res.send(imageName) // send a random image name
+    let selectedImage = imageNameList[Math.floor(Math.random() * imageNameList.length)]
+    let imageURL = 'http://localhost:' + port + '/images/' + selectedImage + '.jpeg'
+    res.send(imageURL) // send a random image name
 })
 
 app.get('/images/:id', (req, res) => {
@@ -43,13 +42,14 @@ app.post('/pathHistory', (req, res) => {
     // REMEMBER: the data in body is in JSON format
 
     // add each obj to the pathHistory
-    for (const obj in req.body) {
-        pathHistory.push(obj)
+    for (let i = 0; i < req.body.length; i++) {
+        pathHistory.push(req.body[i])
     }
 
     res.send("pathHistory length = " + pathHistory.length) // respond with the new length of pathHistory
+    console.log(pathHistory)
 })
 
 app.listen(port, () => {
-    console.log(`Example app running on port ${port}`);
+    console.log(`CNC running on port ${port}`);
 })
