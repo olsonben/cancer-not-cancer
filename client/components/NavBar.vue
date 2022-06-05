00608855f844758d11859eb09fe55f0b73ed9583
Ben Olson <ben.k.olson@gmail.com>
2022-06-05 05:19:11 +0000

    Added default layout with navbar + tabs between admin pages

---
+++ client/components/NavBar.vue
@@ -0,0 +1,54 @@
+<template>
+    <nav class="navbar is-primary" role="navigation" aria-label="main navigation">
+        <div class="navbar-brand">
+            <a class="navbar-item" href="https://bulma.io">
+                <img class='logo' src="https://bulma.io/images/bulma-logo.png">
+            </a>
+
+            <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+            </a>
+        </div>
+
+        <div id="navbarBasicExample" class="navbar-menu">
+            <div class="navbar-start">
+                <a class="navbar-item">
+                    Home
+                </a>
+
+                <nuxt-link to='/pathapp' class="navbar-item">
+                    HotOrNot
+                </nuxt-link>
+
+                <div class="navbar-item has-dropdown is-hoverable">
+                    <a class="navbar-link">More</a>
+
+                    <div class="navbar-dropdown">
+                        <nuxt-link to='/about' class="navbar-item">About</nuxt-link>
+                        <nuxt-link to='/admin/images' class="navbar-item">Admin</nuxt-link>
+                        <hr class="navbar-divider">
+                        <a class="navbar-item">Report an issue</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="navbar-end">
+                <div class="navbar-item">
+                    <div class="buttons">
+                        <nuxt-link to='/login' class="button is-light">
+                            Log in
+                        </nuxt-link>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<style>
+img.logo {
+    width: fit-content;
+}
+</style>
---
+++ client/layouts/default.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <NavBar />
+        <nuxt />
+    </div>
+</template>
