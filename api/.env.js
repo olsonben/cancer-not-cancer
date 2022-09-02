4bc0293f2a44e8b95cbcb19a53bcb1e531ec5ace
Ben Olson <ben.k.olson@gmail.com>
2022-09-02 06:47:40 +0000

    I'm tired, but things should get saved

--- api/.env.js
+++ api/.env.js
@@ -2,7 +2,7 @@ export default {
     port: 5000,
     
     url: {  // These will eventually be largely seperate routes
-        image: 'https://api.milmed.ai', // Could point anywhere on the internet
+        image: 'https://static.milmed.ai', // Could point anywhere on the internet
         base: 'https://api.milmed.ai'
     }
 }
