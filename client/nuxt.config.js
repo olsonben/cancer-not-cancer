b11b6abf502c844c0deeb8f514d3aa48192a8e79
Colin <colin@milmed.ai>
2023-05-02 12:54:37 -0700

    Clean logo.svg, add favicon compatabilities

--- client/nuxt.config.js
+++ client/nuxt.config.js
@@ -24,11 +24,18 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { name: 'format-detection', content: 'telephone=no' },
+      { name: 'msapplication-TileColor', content: '#5d6770' },
+      { name: 'theme-color', content: '#5d6770' }
     ],
     link: [
-      // Small logo for the tab
-      { rel: 'icon', type: 'image/svg+xml', href: '/logo.svg' }
+      // Favicon
+      { rel: 'icon', type: 'image/svg+xml', href: '/logo.svg' },
+      { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#9f262c' },
+      { rel: 'apple-touch-icon', sizes: '180x180', href:"/apple-touch-icon.png" },
+      { rel:'icon', type:'image/png', sizes:'32x32', href:'/favicon-32x32.png' },
+      { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+      { rel: 'manifest', href: '/site.webmanifest' }
     ]
   },
 
