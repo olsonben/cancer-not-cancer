a3dfe75b0c8bf39329eaef4ff472b28cc06144e7
Colin <colin@milmed.ai>
2023-07-21 16:59:38 -0700

    Add custom icons to tasks view

--- client/pages/admin/tasks.vue
+++ client/pages/admin/tasks.vue
@@ -3,8 +3,48 @@
         <!-- Main tasks -->
         <section class='section tasks-controller'>
             <h1 class='title'>Tasks</h1>
-            <div>
-                Create Tasks Here
+            <div class="pb-2">
+                <form @submit.prevent='createTask()'>
+                    <div class="field is-horizontal">
+                        <div class="field-label is-normal">
+                            <label class="label">Create New Task</label>
+                        </div>
+                        <div class="field-body">
+                            <div class="field is-grouped">
+                            <p class="control has-icons-left">
+                                <input class="input" type="text" placeholder="name" v-model="task.name">
+                                <span class="icon is-small is-left">
+                                <i class="cnc-label-tag"></i>
+                                </span>
+                            </p>
+                            <p class="control is-expanded has-icons-left has-icons-right">
+                                <input class="input" type="text" placeholder="Prompt" v-model="task.prompt" />
+                                <span class="icon is-small is-left">
+                                <i class="cnc-question"></i>
+                                </span>
+                            </p>
+                            <p class="control">
+                                <input class='button is-primary' type="submit" value="Create" />
+                            </p>
+                            </div>
+                        </div>
+                    </div>
+                   
+                </form>
+            </div>
+            <div class="py-2">
+                <div class='box'>
+                    <h3>Existing Tasks</h3>
+                    <table class="table is-striped is-narrow">
+                        <thead>
+                            <th v-for="colName in columns">{{ colName }}</th>
+                        </thead>
+                        <tbody>
+
+                            <Row v-for="row in taskData" :key="row[indexProp]" :class="{ 'is-selected': false }"  :row="row" :order="order"/>
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </section>
     </div>
@@ -15,6 +55,31 @@
 export default {
     data() {
         return {
+            task: {
+                name: null,
+                prompt: null,
+            },
+            columns: ['Name', 'Prompt', 'Image Count', 'User Count', 'Progress', ''],
+            order: ['short_name', 'prompt', 'image_count', 'user_count', 'progress'],
+            indexProp: 'id',
+            taskData: [
+                {
+                    'id': 1,
+                    'short_name': 'task 1',
+                    'prompt': 'Is this my question?',
+                    'image_count': 54,
+                    'user_count': 3,
+                    'progress': '<progress max="100" class="progress is-small is-primary" value="45">45</progress>'
+                },
+                {
+                    'id': 2,
+                    'short_name': 'another_task_beta_check_240623',
+                    'prompt': 'Does this have alpha 1 marker properties?',
+                    'image_count': 212,
+                    'user_count': 6,
+                    'progress': '<progress max="100" class="progress is-small is-primary" value="72">72</progress>'
+                }
+            ],
         }
     },
     computed: {
@@ -34,6 +99,10 @@ export default {
     },
 
     methods: {
+        createTask() {
+            console.log('Submit task')
+            console.log(this.task)
+        }
     }
 }
 </script>
@@ -42,6 +111,10 @@ export default {
 
 section.tasks-controller {
     padding-top: 0;
+}
+
+input:focus + span {
+    color: hsl(0, 0%, 4%);
 }
 
 </style>
