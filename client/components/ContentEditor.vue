16c4775c64622389bbcd84c8c0b5ed852df0fda1
Colin <colin@milmed.ai>
2024-02-21 17:29:12 -0800

    WIP: Add tiptap, working though no button controls or css.

--- client/components/ContentEditor.vue
+++ client/components/ContentEditor.vue
@@ -1,21 +1,41 @@
 <script setup>
+import { useEditor, EditorContent } from '@tiptap/vue-3'
+import StarterKit from '@tiptap/starter-kit'
+
+
 const props = defineProps({
     initialContent: { type: String, default: '' },
 })
 
-const content = ref(props.initialContent)
+// const content = ref(props.initialContent)
 
 const emit = defineEmits(['update'])
-
-watch(content, async (newContent, oldContent) => {
-    emit('update', newContent)
+const editor = useEditor({
+  content: props.initialContent,
+  extensions: [
+    StarterKit,
+  ],
+  onUpdate: () => {
+    emit('update', editor.value.getHTML())
+  }
 })
+
+
+// watch(content, async (newContent, oldContent) => {
+//     emit('update', newContent)
+// })
+// watch(editor, async (newContent, oldContent) => {
+//   console.log('editor change')
+//   console.log(newContent)
+//     // emit('update', newContent)
+// })
 </script>
 
 <template>
     <div class="field">
       <div class="control">
-        <textarea v-model="content" class="textarea" placeholder="Textarea"></textarea>
+        <!-- <textarea v-model="content" class="textarea" placeholder="Textarea"></textarea> -->
+        <EditorContent :editor="editor" />
       </div>
     </div>
 </template>
